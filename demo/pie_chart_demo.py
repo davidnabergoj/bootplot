5ca94f92ded04ae11215d9367b369ecbcdeb11c9
David Nabergoj <dn9134@student.uni-lj.si>
2022-04-27 16:07:12 +0200

    Pie chart demo

---
+++ demo/pie_chart_demo.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from src import bootstrapped_plot
+
+
+def make_plot_demo_pie(data, ax, n_classes=6):
+    bar_ticks, bar_counts = np.unique(data, return_counts=True)
+    bar_ticks_adjusted = np.zeros(n_classes)
+    for bt, bc in zip(bar_ticks, bar_counts):
+        bar_ticks_adjusted[bt] = bc
+    wedges, _ = ax.pie(x=bar_ticks_adjusted)
+    ax.legend(wedges, list(range(n_classes)), loc='upper right', bbox_to_anchor=(1.1, 1.1, 0, 0), shadow=True,
+              title='Class')
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+
+    plt.rcParams["figure.figsize"] = (5, 5)
+
+    dataset = np.random.binomial(5, 0.5, size=200)
+    mat = bootstrapped_plot(make_plot_demo_pie, dataset, m=100, out_file='bootstrapped_pie_chart.png')
+
+    plt.figure()
+    plt.matshow(mat)
+    plt.axis('off')
+    plt.show()
