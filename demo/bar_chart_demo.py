896f2d8841a1ea2de85c213e1313155704b46386
David Nabergoj <davidnabergoj4@gmail.com>
2022-05-02 21:48:19 +0200

    Update demo

--- demo/bar_chart_demo.py
+++ demo/bar_chart_demo.py
@@ -21,9 +21,9 @@ if __name__ == '__main__':
 
     plt.rcParams["figure.figsize"] = (5, 5)
 
-    dataset = np.random.binomial(N_CLASSES, 0.2, size=1000)
+    dataset = np.random.binomial(N_CLASSES, 0.3, size=1000)
 
-    bootstrapped_animation(make_plot_demo_bar, dataset, m=400, out_file='bootstrapped_bar_chart.gif', decay=True)
+    bootstrapped_animation(make_plot_demo_bar, dataset, m=400, out_file='bootstrapped_bar_chart.gif')
     mat = bootstrapped_plot(make_plot_demo_bar, dataset, m=100, out_file='bootstrapped_bar_chart.png')
 
     plt.figure()
