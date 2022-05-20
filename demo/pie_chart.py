107d35db7df06d166d28d7daed6f25acc1dca071
David Nabergoj <davidnabergoj4@gmail.com>
2022-05-20 21:01:12 +0200

    Add demos

---
+++ demo/pie_chart.py
@@ -0,0 +1,40 @@
+import numpy as np
+from bootplot import bootplot
+
+
+def make_pie_chart(data_subset, data_full, ax):
+    # Get info from the full dataset
+    n_classes = len(np.unique(data_full))
+
+    # Plot subset
+    ticks, counts = np.unique(data_subset, return_counts=True)
+    ticks_adjusted = np.zeros(n_classes)
+    ticks_adjusted[ticks] = counts
+    wedges, _ = ax.pie(x=ticks_adjusted, explode=0.1 * np.ones(n_classes))
+
+    # Define global axis settings
+    ax.set_xlim(-4, 4)
+    ax.set_ylim(-4, 4)
+    ax.legend(
+        wedges,
+        list(range(n_classes)),
+        loc='upper right',
+        bbox_to_anchor=(1.1, 1.1, 0, 0),
+        shadow=True,
+        title='Class'
+    )
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+
+    dataset = np.random.binomial(7, 0.2, size=1000)
+
+    bootplot(
+        make_pie_chart,
+        dataset,
+        m=100,
+        output_image_path='bootstrapped_pie_chart.png',
+        output_animation_path='bootstrapped_pie_chart.gif',
+        verbose=True
+    )
---
+++ demo/polynomial_regression.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from bootplot import bootplot
+
+
+def make_polynomial_regression(data_subset, data_full, ax):
+    # Plot full dataset
+    ax.scatter(data_full[:, 0], data_full[:, 1])
+
+    # Plot regression model trained on the subset
+    poly = PolynomialFeatures(degree=2)
+    features = poly.fit_transform(data_subset[:, 0].reshape(-1, 1))
+    lr = LinearRegression()
+    lr.fit(features, data_subset[:, 1])
+
+    xs = np.linspace(-10, 10, 1000)
+    xs_features = poly.transform(xs.reshape(-1, 1))
+    ax.plot(xs, lr.predict(xs_features), c='r')
+
+    # Define global axis settings
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+
+    dataset = np.random.randn(100, 2)
+    noise = np.random.randn(len(dataset)) * 2.5
+    dataset[:, 1] = dataset[:, 0] * 1.5 + 2 + noise
+
+    bootplot(
+        make_polynomial_regression,
+        dataset,
+        m=100,
+        output_image_path='bootstrapped_polynomial_regression.png',
+        output_animation_path='bootstrapped_polynomial_regression.gif',
+        verbose=True
+    )
