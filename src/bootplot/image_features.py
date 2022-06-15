8a60d9fbb80262f318148b1f89c5734cd80d2ea4
David Nabergoj <davidnabergoj4@gmail.com>
2022-06-15 22:23:18 +0200

    Add docstrings

--- src/bootplot/image_features.py
+++ src/bootplot/image_features.py
@@ -4,13 +4,25 @@ from skimage.feature import hog
 import cv2
 
 
-def compute_hog_features(gray_images):
+def compute_hog_features(gray_images: np.ndarray) -> np.ndarray:
+    """
+    Compute features based on histograms of oriented gradients (HOG) for a given set of images.
+
+    :param gray_images: input images.
+    :return: features for each image.
+    """
     hog_features = np.stack([hog(im, cells_per_block=(1, 1), pixels_per_cell=(10, 10)) for im in gray_images])
     hog_features = (hog_features - np.min(hog_features)) / (np.max(hog_features) - np.min(hog_features))
     return hog_features
 
 
-def compute_center_of_mass(gray_images):
+def compute_center_of_mass(gray_images: np.ndarray) -> np.ndarray:
+    """
+    Compute centers of mass for all input images.
+
+    :param gray_images: input images.
+    :return: center of mass for each image.
+    """
     centers_of_mass = np.zeros((len(gray_images), 2))
     for i in range(len(gray_images)):
         centers_of_mass[i] = scipy.ndimage.center_of_mass(gray_images[i])
@@ -18,7 +30,15 @@ def compute_center_of_mass(gray_images):
     return centers_of_mass
 
 
-def compute_center_of_mass_per_component(gray_images, threshold=1e-5):
+def compute_center_of_mass_per_component(gray_images: np.ndarray, threshold: float = 1e-5) -> np.ndarray:
+    """
+    Compute centers of mass for all input images.
+    For each image, we first binarize it, determine its components, then compute the center mass for each component.
+
+    :param gray_images: input images.
+    :param threshold: binarization threshold.
+    :return: center of mass for each component of each image.
+    """
     movement_mask = (np.var(gray_images, axis=0) > threshold).astype(np.uint8)
     n_components, label_mask, stats, centroids = cv2.connectedComponentsWithStats(movement_mask)
     centers = np.zeros((len(gray_images), 2 * n_components - 2))
